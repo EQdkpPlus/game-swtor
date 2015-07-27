d42852b5fae276ef79ee50839e6d2e8afa003ca0
lupus0 <mail.sir.lupus@gmail.com>
2015-07-27 20:17:13 +0200

    Update swtor.class.php
    Added Togruta Race

--- swtor.class.php
+++ swtor.class.php
@@ -57,8 +57,8 @@ if(!class_exists('swtor')) {
 				'decorate'	=> true,
 				'parent'	=> array(
 					'faction' => array(
-						'republic'	=> array(0,1,2,3,4,5,6,7,8,9,10),
-						'imperial'	=> array(0,1,2,3,4,5,6,7,8,9,10),
+						'republic'	=> array(0,1,2,3,4,5,6,7,8,9,10,11),
+						'imperial'	=> array(0,1,2,3,4,5,6,7,8,9,10,11),
 					),
 				),
 			),
